82f89281b8cf7faa5fa2a1f0af86025a49bca269
marasmadwa <michalkowski.m@gmail.com>
2018-03-18 14:12:54 +0100

    initial commit

---
+++ beer-app/gulpfile.js
@@ -0,0 +1,36 @@
+const gulp = require("gulp");
+const sass = require('gulp-sass');
+const plumber = require('gulp-plumber');
+const autoprefixer = require('gulp-autoprefixer');
+const sourcemaps = require('gulp-sourcemaps');
+const gutil = require('gulp-util');
+
+const showErrors = function (err) {
+    console.log(gutil.colors.red(err.toString()));
+    this.emit('end');
+};
+
+gulp.task('sass', function () {
+    return gulp.src('./src/scss/main.scss')
+        .pipe(plumber({
+            errorHandler: showErrors
+        }))
+        .pipe(sourcemaps.init())
+        .pipe(sass({
+            outputStyle: "compressed"
+        }))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./src/css'))
+});
+
+gulp.task('watch', function () {
+    gulp.watch('./scss/**/*.scss', ['sass']);
+});
+gulp.task('default', function () {
+    console.log('-------start pracy -----------');
+    gulp.start(["sass", "watch"]);
+});
---
+++ beer-app/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class App extends React.Component {
+  render() {
+    return (
+     <div className="app">The app will be ready soon :)</div>
+    );
+  }
+}
